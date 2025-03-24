f54d837d13e227419d5ca6696902f0b69d6c143b
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-03-24 06:08:22 +0200

    inject service to eachother

---
+++ Services/src/app/tasks/tasks.service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly();
+  private loggingService = inject(LoggingService);
+  addTasks(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log('Added Task with title ' + taskData.title);
+  }
+  updateTaskStatus(TaskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === TaskId ? { ...task, status: newStatus } : task
+      )
+    );
+  }
+}
