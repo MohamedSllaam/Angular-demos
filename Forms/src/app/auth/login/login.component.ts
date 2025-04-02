22ed6c21102e98833fe7f8bbdde6209bf88a600b
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-04-02 15:47:20 +0200

    add Tameltate-form

--- Forms/src/app/auth/login/login.component.ts
+++ Forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { debounce, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) => {
+            console.log(value.email);
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({ email: value.email })
+            );
+          },
+        });
+
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+    const enteredEmsil = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+    console.log(formData.form);
+    formData.form.reset();
+    console.log(enteredEmsil, enteredPassword);
+  }
+}
